8b6146735265e92eef151be4d8b7738753f37bb2
chaeveen <167851540+chaeveen@users.noreply.github.com>
2025-04-06 23:58:25 +0900

    Create footer.php

---
+++ web/gidx/inc/footer.php
@@ -0,0 +1,43 @@
+<footer>
+    <div class="footer_top">
+        <div class="wFix d_flex j-c_space-between">
+            <ul class="policy">
+                <li><a href="javascript:void(0);">사이트이용약관</a></li>
+                <li><a href="javascript:void(0);">개인정보 처리방침</a></li>
+                <li><a href="javascript:void(0);">이메일 무단 수집 거부</a></li>
+                <li><a href="javascript:void(0);">사이트맵</a></li>
+            </ul>
+            <div class="link">
+                <select>
+					<option>협력웹사이트</option>
+					<option>협력웹사이트</option>
+				</select>
+            </div>
+        </div>
+    </div>
+    <div class="footer_mid">
+        <div class="wFix d_flex">
+			<h1 class="logo"><a href="../contents/main.php">경북산업DX협업지원센터</a></h1>
+            <div class="info_area">
+                <address>주소 : (39177) 경상북도 구미시 대학로 61, 공동실험실습관 303호   대표전화 : 054-478-7878
+				</address>
+                <cite>COPYRIGHT 2024 GIDX. ALL RIGHTS RESERVED.</cite>
+            </div>
+        </div>
+    </div>
+	<div class="footer_bottom">
+		<div class="wFix">
+			<ul class="d_flex">
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo1.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo2.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo3.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo4.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo5.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo6.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo7.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo8.png" alt="logo"></a></li>
+				<li><a href="javascript:void(0);"><img src="../images/main/f_logo9.png" alt="logo"></a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
