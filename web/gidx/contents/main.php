2610a1f05ec0b7af313a17869fd9fbb7128a5c46
chaeveen <167851540+chaeveen@users.noreply.github.com>
2025-04-06 23:25:01 +0900

    Create main.php

---
+++ web/gidx/contents/main.php
@@ -0,0 +1,396 @@
+<?php include_once('../inc/doctype.php'); ?>
+<?php include_once('../inc/header.php'); ?>
+
+<main class="main p_relative">
+    <div class="main_visual">
+        <div class="video">
+            <video class="main_video" autoplay="" muted="" loop="" playsinline="">
+                <source src="../images/main/main_video.mp4" type="video/mp4">
+            </video>
+            <div class="dimBg"></div>
+            <div class="txt">
+                <p>경북 기업이 산업데이터 활용을 통해<br>
+                국내 주력산업과 신산업의 디지털 전환을 <br class="mobile">촉진·확산할 수 있도록<br>
+                <em>경북산업DX협업<br class="mobile">지원센터</em>가<br>
+                <b>산업 디지털 대전환<br>종합지원서비스</b>를 지원합니다.</p>
+            </div>
+        </div>
+        <div class="q_menu">
+            <a href="sub2-1.php" class="box">
+                <h3>업종별 맞춤형 DX<br>
+                개발 적용</h3>
+                <p>경북지역 전자IT융합, 첨단소재,<br>
+                모빌리티 산업을 중심으로<br>
+                DX추진전략과 선도사업을<br>
+                발굴/지원합니다.</p>
+            </a>
+            <a href="sub2-2.php" class="box">
+                <h3>융합 얼라이언스<br>
+                구축 운영</h3>
+                <p>경북지역 산업 디지털 전환<br>
+                생태계 조성을 위한<br>
+                DX얼라이언스를<br>
+                구축 및 운영하고 있습니다.</p>
+            </a>
+            <a href="sub2-3.php" class="box">
+                <h3>다양한 지역형 DX 사업<br>
+                지원 추진</h3>
+                <p>지역산업 디지털<br>
+                밸류체인망 구축 등<br>
+                다양한 지원사업을<br>
+                추진하고 있습니다.</p>
+            </a>
+            <a href="sub2-4.php" class="box">
+                <h3>지역 내 연구·지원<br>
+                기관 연계</h3>
+                <p>지역 내 다양항 연구 및<br>
+                지원기관과 연계하여<br>
+                산업 디지털 전환을<br>
+                리딩하겠습니다.</p>
+            </a>
+        </div>
+    </div>
+    <!-- main_visual 끝 -->
+    
+    <div class="corporation">
+        <div class="wFix">
+            <div class="tit_area">
+                <h3 class="tit_st1 mb20">corporation</h3>
+                <h2 class="tit_st2">얼라이언스 참여기업</h2>
+            </div>
+            <div class="tabs mt30 mb40">
+                <a href="javascript:void(0);" class="tab active" data-tab1="tab_1">전자·IT융합</a>
+                <a href="javascript:void(0);" class="tab" data-tab1="tab_2">첨단소재</a>
+                <a href="javascript:void(0);" class="tab" data-tab1="tab_3">모빌리티</a>
+            </div>
+
+            <!-- Swiper -->
+            <div id="tab_1" class="swiper slider-schedule tab_content active o_hidden p15">
+                <div class="swiper-wrapper mb50">
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide1.png" alt="logo">
+                            </div>
+                            <h4 class="tit">건테크</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : SiC, 세라믹, Quartz</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide2.png" alt="logo">
+                            </div>
+                            <h4 class="tit">다이나톤</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : 디지털 피아노, 포터블 피아노, 미디 키보드</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide3.png" alt="logo">
+                            </div>
+                            <h4 class="tit">비즈니움</h4>
+                            <div class="txt">
+                                <p>응용소프트웨어 개발 및 공급원</p>
+                                <p>주생산품 : 응용소프트웨어, 학술연구용역, 연구개발 컨설팅</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                </div><!-- \\swiper-wrapper -->
+                <div class="slide_control">
+                    <div class="swiper-pagination"></div>
+                    <div class="btns">
+                        <div class="prev_btn bt"><span class="material-icons">chevron_left</span></div>
+                        <div class="next_btn bt"><span class="material-icons">chevron_right</span></div>
+                        <a href="sub3-3.php" class="more_btn bt"><span class="material-icons">add</span></a>
+                    </div>
+                </div>
+            </div><!-- \\tab_1 -->
+
+			<!-- Swiper -->
+            <div id="tab_2" class="swiper slider-schedule2 tab_content o_hidden p15">
+                <div class="swiper-wrapper mb50">
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide1.png" alt="logo">
+                            </div>
+                            <h4 class="tit">건테크</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : SiC, 세라믹, Quartz</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide2.png" alt="logo">
+                            </div>
+                            <h4 class="tit">다이나톤</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : 디지털 피아노, 포터블 피아노, 미디 키보드</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide3.png" alt="logo">
+                            </div>
+                            <h4 class="tit">비즈니움</h4>
+                            <div class="txt">
+                                <p>응용소프트웨어 개발 및 공급원</p>
+                                <p>주생산품 : 응용소프트웨어, 학술연구용역, 연구개발 컨설팅</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                </div><!-- \\swiper-wrapper -->
+                <div class="slide_control">
+                    <div class="swiper-pagination"></div>
+                    <div class="btns">
+                        <div class="prev_btn bt"><span class="material-icons">chevron_left</span></div>
+                        <div class="next_btn bt"><span class="material-icons">chevron_right</span></div>
+                        <div class="more_btn bt"><span class="material-icons">add</span></div>
+                    </div>
+                </div>
+            </div><!-- \\tab_2 -->
+
+			<!-- Swiper -->
+            <div id="tab_3" class="swiper slider-schedule3 tab_content o_hidden p15">
+                <div class="swiper-wrapper mb50">
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide1.png" alt="logo">
+                            </div>
+                            <h4 class="tit">건테크</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : SiC, 세라믹, Quartz</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide2.png" alt="logo">
+                            </div>
+                            <h4 class="tit">다이나톤</h4>
+                            <div class="txt">
+                                <p>전자/전기</p>
+                                <p>주생산품 : 디지털 피아노, 포터블 피아노, 미디 키보드</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide3.png" alt="logo">
+                            </div>
+                            <h4 class="tit">비즈니움</h4>
+                            <div class="txt">
+                                <p>응용소프트웨어 개발 및 공급원</p>
+                                <p>주생산품 : 응용소프트웨어, 학술연구용역, 연구개발 컨설팅</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                    <div class="swiper-slide">
+                        <a href="javascript:void(0);" class="slider-content">
+                            <!-- <span class="tag">수요기업</span> -->
+                            <div class="img_wrap">
+                                <img src="../images/main/slide4.png" alt="logo">
+                            </div>
+                            <h4 class="tit">세정로봇</h4>
+                            <div class="txt">
+                                <p>기계</p>
+                                <p>주생산품 : 자동화기계</p>
+                            </div>
+                        </a>
+                    </div><!-- \\swiper-slide -->
+                </div><!-- \\swiper-wrapper -->
+                <div class="slide_control">
+                    <div class="swiper-pagination"></div>
+                    <div class="btns">
+                        <div class="prev_btn bt"><span class="material-icons">chevron_left</span></div>
+                        <div class="next_btn bt"><span class="material-icons">chevron_right</span></div>
+                        <div class="more_btn bt"><span class="material-icons">add</span></div>
+                    </div>
+                </div>
+            </div><!-- \\tab_3 -->
+        </div>
+    </div>
+    <!-- corporation 끝 -->
+
+    <div class="news">
+        <div class="wFix">
+            <div class="tit_area">
+                <h3 class="tit_st1 mb20">news</h3>
+                <h2 class="tit_st2">DX 뉴스</h2>
+            </div>
+            <div class="d_flex mt50">
+                <a href="view.php" class="con">
+                    <div class="img_wrap">
+                        <img src="../images/main/news2.png" alt="news">
+                    </div>
+                    <div class="txt_wrap">
+                        <h4 class="tit ellipsis2">경상북도, 제조기업의 디지털 전환 가속화 기술혁신
+                        포럼 개최</h4>
+                        <div class="txt">
+                            <p>2024-07-01</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="view.php" class="con">
+                    <div class="img_wrap">
+                        <img src="../images/main/news1.png" alt="news">
+                    </div>
+                    <div class="txt_wrap">
+                        <h4 class="tit ellipsis2">경북도 "구글 손잡고 AI 전문인재 키운다"</h4>
+                        <div class="txt">
+                            <p>2024-06-26</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="view.php" class="con">
+                    <div class="img_wrap">
+                        <img src="../images/main/news3.png" alt="news">
+                    </div>
+                    <div class="txt_wrap">
+                        <h4 class="tit ellipsis2">[5차 산업혁명 특집③] ICT융합만이 살 길이다,
+                        국내외 기업들의 디지털전환(DX) 사례들</h4>
+                        <div class="txt">
+                            <p>2024-06-17</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!-- news 끝 -->
+
+    <div class="notice">
+        <div class="wFix">
+            <h2 class="tit_st2 font_white mb10">사업공고</h2>
+            <a href="view.php" class="con">
+                <h3 class="tit">(통합공고)DX 지원 컨설팅 수혜기업 모집 공고</h3>
+                <span class="material-icons font_white">arrow_forward</span>
+            </a>
+        </div>
+    </div>
+    <!-- notice 끝 -->
+
+    <div class="bg_txt">
+        GYEONGBUK INDUSTRY DX COOPERATION CENTER
+    </div>
+</main>
+
+<?php include_once('../inc/footer.php'); ?>
