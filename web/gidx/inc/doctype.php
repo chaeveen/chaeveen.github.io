23de31915520214b9c7e5a5d3893463a5fe0b352
chaeveen <167851540+chaeveen@users.noreply.github.com>
2025-04-06 23:58:41 +0900

    Add files via upload

---
+++ web/gidx/inc/doctype.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../images/common/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="../images/common/favicon.ico" type="image/x-icon">
+    <meta property="og:url" content="http://sian.knlsys.com/gidx/contents/main.php">
+    <meta property="og:title" content="경북산업DX협업지원센터">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="../images/common/share.png">
+    <meta property="og:description" content="경북산업DX협업지원센터 홈페이지입니다.">
+    <title>경북산업DX협업지원센터</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/font.css">
+    <link rel="stylesheet" href="../css/common.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/sub.css">
+
+    <!-- jquery -->
+    <script src="../js/jquery/jquery-1.11.2.min.js"></script>
+    <script src="../js/web.js"></script>
+
+    <!-- swiper -->
+    <link rel="stylesheet" href="../js/swiper/swiper.min.css">
+    <script src="../js/swiper/swiper.min.js"></script>
+    
+</head>
